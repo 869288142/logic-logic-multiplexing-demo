f40d23dd7ad2973d294a2226ea87dac556c3d82c
869288142 <869288142@qq.com>
2021-01-17 16:23:49 +0800

    初始化Angular11、React17、Vue3

---
+++ vue3-hook/src/App.tsx
@@ -0,0 +1,11 @@
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  render(){
+    return (<>
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </>)
+  }
+})
